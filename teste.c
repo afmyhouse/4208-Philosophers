7f64d4f5161a01f18a78ccde0597bb90c09cfd11
Antonio Ferreira <af.myhouse@gmail.com>
2023-11-12 21:10:19 +0000

    HP 20231112 002 update teste.c

--- teste.c
+++ teste.c
@@ -3,24 +3,23 @@
 int main(void)
 {
 	printf("Hello, world!\n");
-	int *f[10];
-	int g[10] = {1,2,3,4,5,6,7,8,9,10};
+	char *f[10];
+	char g[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-	int i = 1;
-	while ( i <= 10)
+	int i = 0;
+	while ( i < 10)
 	{
-		printf("f[%d] = %ls\n", i, f[i]);
-		printf("g[%d] = %d\n", i, g[i]);
-		f[(i - 1) % 2] = &g[i - 1];
-		f[i % 2] = &g[i];
+		printf("f[%d] = %p\n", i, f[i]);
+		printf("g[%d] = %c\n", i, g[i]);
+		f[i] = &g[i];
 		i++;
 	}
 
-	i = 1;
-	while ( i <= 10)
+	i = 0;
+	while ( i <  10)
 	{
-		printf("f[%d] = %ls\n", i, f[i]);
-		printf("g[%d] = %d\n", i, g[i]);
+		printf("f[%d] = %p\n", i, f[i]);
+		printf("g[%d] = %c\n", i, g[i]);
 		i++;
 	}
 
